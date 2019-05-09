5a62b5b8f496ee70221540ebcb3aafeacf0d8054
Alex Pino <alex@Alexs-MacBook-Pro.local>
2019-05-09 11:33:24 -0400

    database set up

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name) VALUES ("A good manchego is hard to find burger");
+INSERT INTO burgers (burger_name) VALUES ("Poutine on the ritz burger");
+INSERT INTO burgers (burger_name) VALUES ("Hummus a tune burger");
