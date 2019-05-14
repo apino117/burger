9110e98931e07ba30849cdfcf03358c154d4954c
Alex Pino <alex@Alexs-MacBook-Pro.local>
2019-05-14 10:25:20 -0400

    post requests work in postman

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -16,16 +16,16 @@ router.get("/", function (req, res) {
     });
 });
 
-// router.post("/", function (req, res) {
-//     burger.create([
-//         "burger_name"
-//     ], [
-//             req.body.burger_name
-//         ], function (result) {
-//             // Send back the ID of the new quote
-//             res.json({ id: result.insertId });
-//         });
-// });
+router.post("/api/burgers", function (req, res) {
+    burger.create([
+        "burger_name", "devoured"
+    ], [
+            req.body.burger_name, req.body.devoured
+        ], function (result) {
+            // Send back the ID of the new quote
+            res.json({ id: result.insertId });
+        });
+});
 
 // router.put("/api/cats/:id", function (req, res) {
 //     var condition = "id = " + req.params.id;
