dfe6077085e0a0366dd5e026183f7c27e050f52e
Alex Pino <alex@Alexs-MacBook-Pro.local>
2019-05-14 10:36:36 -0400

    post from form successfull

--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -3,27 +3,31 @@ $(function () {
 
     // --------- FORM SUBMIT BUTTON --------- //
 
-    // $(".create-form").on("submit", function(event) {
-    //   // Make sure to preventDefault on a submit event.
-    //   event.preventDefault();
+    $(".create-form").on("submit", function (event) {
+        // Make sure to preventDefault on a submit event.
+        event.preventDefault();
 
-    //   var newCat = {
-    //     name: $("#ca").val().trim(),
-    //     sleepy: $("[name=sleepy]:checked").val().trim()
-    //   };
 
-    //   // Send the POST request.
-    //   $.ajax("/api/cats", {
-    //     type: "POST",
-    //     data: newCat
-    //   }).then(
-    //     function() {
-    //       console.log("created new cat");
-    //       // Reload the page to get the updated list
-    //       location.reload();
-    //     }
-    //   );
-    // });
+
+        var newBurger = {
+            burger_name: $("#burger-name").val().trim(),
+            devoured: 0
+        }
+
+        console.log(newBurger);
+
+        // Send the POST request.
+        $.ajax("/api/burgers", {
+            type: "POST",
+            data: newBurger
+        }).then(
+            function () {
+                console.log("created new burger");
+                // Reload the page to get the updated list
+                location.reload();
+            }
+        );
+    });
 
     // --------- DEVOUR BUTTON --------- //
 
